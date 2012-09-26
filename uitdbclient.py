62fb5f820005532503067bf79504001134cfe808
Luk Verhelst <luk.verhelst@gmail.com>
2012-09-26 10:21:37 +0200

    new methods for adding keywords and translations to items

--- uitdbclient.py
+++ uitdbclient.py
@@ -9,15 +9,18 @@ from xml.dom.minidom import Document
 from xml.sax.handler import ContentHandler
 
 # argparser:
-# Allows command line arguments 
-# Print usage when no args or when arg -h is provided 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="upload CdbXML doc from your filesystem")
-parser.add_argument("-u", "--url", help="import CdbXML from url")
 parser.add_argument("-id", "--cdbid", help="ID of object to perform operation on")
 parser.add_argument("-v", "--verbosity", help="print parser outcome to screen", action="store_true")
 parser.add_argument("-s", "--save_report", help="save parser outcome to file", action="store_true")
 parser.add_argument("-d", "--delete", help="delete item, use with --cdbid", action="store_true")
+parser.add_argument("-k", "--keywords", help="add keywords, use with --cdbid")
+parser.add_argument("-o", "--obj_type", help="choose: event, actor, production")
+parser.add_argument("-l", "--lang", help="translation: to nl, fr, en, use with --cdbid")
+parser.add_argument("-td", "--title", help="title translation, use with --lang")
+parser.add_argument("-sd", "--shortdescription", help="short description translation, use with --lang")
+parser.add_argument("-ld", "--longdescription", help="long description translation, use with --lang")
 
 if len(sys.argv)==1:
   parser.print_help()
@@ -30,67 +33,99 @@ def fileupload(filename):
 
   try:
     doc = xml.dom.minidom.parse(filename)
-    _process_input(doc)
+    _process_content_objects(doc)
   except Exception, e:
     print "Error occured while parsing XML: %s" % e
     sys.exit(1)
 
   print "PROCESSING FINISHED"
 
-def parseurl(url):  
-  try:  
-    response = urllib2.urlopen(url)
-    data = response.read()
-    response.close()
-  except Exception, e:
-    print "Error occured while opening URL: %s" % e
+def addkeywords(cdbid, keywords):
+  objecttype = _find_object_type(cdbid)
+  path = "/api/v1/" + objecttype + "/" + cdbid + "/keywords"
+  formfields = {'keywords': keywords}
+  headers = {"Content-Length" : len(keywords)}
+  httpmethod = "POST"
+  _modify_content_object(path, headers, formfields, httpmethod)
+      
+def addtranslation(cdbid, lang, formfields):
+  objecttype = _find_object_type(cdbid)
+  
+  if lang:
+    formfields['lang'] = lang
+  else:
+    print "lang is required"
     sys.exit(1)
-
-  try:
-    doc = xml.dom.minidom.parseString(data)
-    _process_input(doc)
-  except Exception, e:
-    print "Error occured while parsing XML: %s" % e
-    sys.exit(1)
-
+  
+  path = "/api/v1/" + objecttype + "/" + cdbid + "/translations"
+  headers = {"Content-Length" : len(formfields)}
+  httpmethod = "POST"
+  _modify_content_object(path, headers, formfields, httpmethod)
+      
 def deleteitem(cdbid):
-  cnt = 0
-  results = []
+  objecttype = _find_object_type(cdbid)
+  path = "/api/v1/" + objecttype + "/" + cdbid
+  formfields = {}
+  headers = {"Content-Length" : len(cdbid)}
+  httpmethod = "DELETE"
+  _modify_content_object(path, headers, formfields, httpmethod)
 
+def _find_object_type(cdbid):
+  if args.obj_type:
+    return args.obj_type
+    
+  objecttypes = ['event', 'actor', 'production']
+  
+  for objecttype in objecttypes:
+    r1 = _connect_to_uitdb()
+    userkey = _get_userkey(r1.read())
+    url = "http://test.rest.uitdatabank.be/api/v1/" + objecttype + "?q=" + cdbid + "&key=" + userkey
+    r2 = urllib2.urlopen(url)
+    doc = xml.dom.minidom.parse(r2)
+    nofrecords = doc.getElementsByTagName("nofrecords")
+    found = nofrecords[0].firstChild.nodeValue  
+  
+    if found:
+      rvalue = objecttype
+      break
+  
+  return rvalue
+
+def _modify_content_object(path, headers, formfields, httpmethod):
   # connect to uitdb
   response = _connect_to_uitdb()
-  #authenticate user
+  # authenticate user
   userkey = _get_userkey(response.read())
-    
+
   if response.getcode() == 200 and userkey:
-      params = urllib.urlencode({'key': userkey})
-      #headers = {"Content-type": "text/xml", "Content-Length" : len(xml_output)}
-
-      conn = httplib.HTTPConnection("test.rest.uitdatabank.be")
-      conn.request("DELETE", "/api/v1/production", params)
-      starttime = time.time()
-      response = conn.getresponse()
-      endtime = time.time()
-
-      duration = round((endtime - starttime) * 1000)
-      data = response.read()
-      conn.close()
-      
-      # Print the response analyses
-      result = {}
-      result['http_response'] = str(response.status) + ' ' + str(response.reason)
-      result['duration_ms'] = str(duration)
-      result['api_response'] = data 
-      
-      if args.verbosity:
-        for key, value in result.items():
-          print key, ':', str(value)
-        print '-----------------------'        
+    formfields['key'] = userkey
+    params = urllib.urlencode(formfields)
+    path = path + "?" + params
+    print path
+    conn = httplib.HTTPConnection("test.rest.uitdatabank.be")
+    conn.request(httpmethod, path, "", headers) 
+    starttime = time.time()
+    response = conn.getresponse()
+    endtime = time.time()
+    duration = round((endtime - starttime) * 1000)
+    data = response.read()
+    conn.close()
+    
+    # Print the response analyses
+    result = {}
+    result['http_response'] = str(response.status) + ' ' + str(response.reason)
+    result['duration_ms'] = str(duration)
+    result['api_response'] = data 
+    
+    if args.verbosity:
+      for key, value in result.items():
+        print key, ':', str(value)
+      print '-----------------------'
   else:
     print "HTTP connection closed with error status: %s" % r1.status
-    sys.exit(1)  
+    sys.exit(1)
 
-def _process_input(doc):
+def _process_content_objects(doc):
   cnt = 0
   results = []
 
@@ -202,11 +237,22 @@ def _connect_to_uitdb():
 def main():
   if args.file:
     fileupload(args.file)
-  if args.url:
-    parseurl(args.url)
+
   if args.cdbid:
     if args.delete:
       deleteitem(args.cdbid)
-  
+    if args.keywords:
+      addkeywords(args.cdbid, args.keywords)
+    if args.lang:
+      formfields = {}
+      
+      if args.title:
+        formfields['title'] = args.title
+      if args.shortdescription:
+        formfields['shortdescription'] = args.shortdescription
+      if args.longdescription:
+        formfields['longdescription'] = args.longdescription
+      addtranslation(args.cdbid, args.lang, formfields)
+
 if __name__ == '__main__':
   main()
